8c13e4d80a41577c4df627476034f1bb6b1805f3
rm-reins <rm_reins@icloud.com>
2025-05-23 18:51:27 +0200

    feat: add custom fonts and global styles for the app

---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const GlobalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    fontFamily: "PoetsenOne-Regular",
+  },
+  bodyText: {
+    fontSize: 16,
+    fontWeight: "normal",
+    fontFamily: "Fredoka-Regular",
+  },
+});
