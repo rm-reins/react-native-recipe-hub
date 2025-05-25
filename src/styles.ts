d9d269493646c5cbd251a46759dc3989a6b17ea1
rm-reins <rm_reins@icloud.com>
2025-05-25 04:10:00 +0200

    style: update global styles

--- src/styles.ts
+++ src/styles.ts
@@ -1,17 +1,33 @@
 import { StyleSheet } from "react-native";
 
-const GlobalColors = StyleSheet.create({
+const GlobalColors = {
   colors: {
-    backgroundColor: "#fff",
+    backgroundColor: "#F6F7F9",
+    black: "#000000",
+    white: "#FFFFFF",
+    primary: "#FF9F43",
   },
-});
+  button: {
+    primary: "#FF9F43",
+    secondary: "#EAEBED",
+  },
+  grey: {
+    100: "#FFFFFF",
+    200: "#F6F7F9",
+    300: "#EAEBED",
+    400: "#D7D8DA",
+    500: "#C4C5C7",
+    600: "#B1B3B6",
+    700: "#9EA0A4",
+    800: "#8B8D91",
+    900: "#787A7E",
+  },
+};
 
 const GlobalStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: GlobalColors.colors.backgroundColor,
-    alignItems: "center",
-    justifyContent: "center",
   },
   headerFont: {
     fontFamily: "Lato-Black",
